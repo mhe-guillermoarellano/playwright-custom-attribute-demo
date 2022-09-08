eae860d9ff61627b1dee7e62488aa1258a86853b
Guillermo Arellano <guillermo.arellano@mheducation.com>
2022-09-08 12:33:55 -0500

    test: add multiple ids test

--- e2e/src/example.spec.ts
+++ e2e/src/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Angular Ng14 Localhost', () => {
+test.describe('Angular Ng14 Localhost Playwright Test', () => {
   test('should locate "data-automation-id" attribute for element selection', async ({
     page,
   }) => {
@@ -32,5 +32,18 @@ test.describe('Angular Ng14 Localhost', () => {
     const dataTestId = page.locator('[href="https://angular.io/tutorial"]');
 
     await expect(dataTestId).toHaveCount(1);
+  });
+
+  test('should test multiple elements with same data ids when configured', async ({
+    page,
+  }) => {
+
+    await page.goto('http://localhost:4200');
+
+    const multipleTestIds = page.locator('[data-automation-id="multiple-data-ids-oh-my"]');
+
+    await expect(multipleTestIds).toHaveCount(2); // Works because Playwright Count method knows how to with multiple elements
+    // uncomment code below to view Playwright error
+    // expect(await multipleTestIds.textContent()).toContain('Here are some links to help you get started'); // Throws because Playwright method can only work with 1 element
   });
 });
